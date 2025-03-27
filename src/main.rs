59dbca538beaeeff83669da2362531a6a84c13a5
yuh-source <zac.zrinski@gmail.com>
2025-03-27 08:57:26 +0800

    changed colours of circles

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::f32::consts::PI;
 
 use macroquad::prelude::*;
 
-const RAY_COUNT: usize = 1000;
+const RAY_COUNT: usize = 150;
 
 struct Circle {
     x: f32,
@@ -17,9 +17,8 @@ struct Ray {
     angle: f32,
 }
 
-
 impl Circle {
-    fn update(&mut self, x : f32, y:f32, r: f32) {
+    fn update(&mut self, x: f32, y: f32, r: f32) {
         self.x = x;
         self.y = y;
         self.r = r;
@@ -36,28 +35,35 @@ impl Circle {
         let w: f32 = screen_width();
         let h: f32 = screen_height();
         let path_radius = h / 2.0 * 0.6;
-        
+
         let time = get_time() as f32;
         let angle = time * 0.5; // controls movement speed
-        
+
         self.x = w / 2.0 + path_radius * angle.cos();
         self.y = h / 2.0 + path_radius * angle.sin();
     }
 
-    fn draw(&self) {
+    fn draw(&self, colour: Color) {
         let r_sqr: f32 = self.r * self.r;
         for x in ((self.x - self.r).floor() as i32)..=((self.x + self.r).ceil() as i32) {
             for y in ((self.y - self.r).floor() as i32)..=((self.y + self.r).ceil() as i32) {
                 let d_sqr: f32 = (x as f32 - self.x).powi(2) + (y as f32 - self.y).powi(2);
                 if d_sqr <= r_sqr {
-                    draw_rectangle(x as f32, y as f32, 1.0, 1.0, WHITE);
+                    draw_rectangle(x as f32, y as f32, 1.0, 1.0, colour);
                 }
             }
         }
     }
 
-    fn generate_rays (&self) -> Vec<Ray> {
-        let mut rays: Vec<Ray> = vec![Ray { x: 0.0, y:0.0, angle:0.0 }; RAY_COUNT];
+    fn generate_rays(&self) -> Vec<Ray> {
+        let mut rays: Vec<Ray> = vec![
+            Ray {
+                x: 0.0,
+                y: 0.0,
+                angle: 0.0
+            };
+            RAY_COUNT
+        ];
 
         for i in 0..RAY_COUNT {
             let angle: f32 = (i as f32 / RAY_COUNT as f32) * 2.0 * PI;
@@ -69,7 +75,7 @@ impl Circle {
             rays[i] = ray;
         }
 
-        return rays;
+        rays
     }
 
     fn draw_rays(&self, shadow_circle: &Circle) {
@@ -88,7 +94,7 @@ impl Circle {
 
                 draw_rectangle(x, y, 1.0, 1.0, WHITE);
 
-                if ( x < 0.0 || x > screen_width()) || (y < 0.0 || y > screen_height()){
+                if (x < 0.0 || x > screen_width()) || (y < 0.0 || y > screen_height()) {
                     break;
                 }
 
@@ -99,30 +105,28 @@ impl Circle {
             }
         }
     }
-
 }
-
 
 #[macroquad::main("raytracing")]
 async fn main() {
     let mut light_circle = Circle {
-        x: screen_width()/4.0,
-        y: screen_height()/2.0,
+        x: screen_width() / 4.0,
+        y: screen_height() / 2.0,
         r: 50.0,
     };
 
     let mut shadow_circle = Circle {
-        x: screen_width()*3.0/4.0,
-        y: screen_height()/2.0,
+        x: screen_width() * 3.0 / 4.0,
+        y: screen_height() / 2.0,
         r: 100.0,
     };
-    
+
     loop {
         clear_background(BLACK);
         draw_fps();
 
-        light_circle.draw();
-        shadow_circle.draw();
+        light_circle.draw(YELLOW);
+        shadow_circle.draw(GRAY);
 
         shadow_circle.circular_movement();
         light_circle.follow_pointer();
